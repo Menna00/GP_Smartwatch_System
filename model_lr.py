d169fa944b7b8254a3ded93112b4c6d9ee34f1c0
No one <abdelrahmanahmed75.ae@gmail.com>
2024-05-29 17:38:32 +0300

    editing the model

--- model_lr.py
+++ model_lr.py
@@ -6,8 +6,8 @@ import scipy
 
 
 # Read the Excel file into a DataFrame
-# data = pd.read_excel(r"G:\\College\\Graduation Project\\model_deployment\\stress_detection\\stress.xlsx")
-data = pd.read_csv("stress.csv")
+data = pd.read_excel(r"G:\\College\\Graduation Project\\model_deployment\\stress_detection\\stress.xlsx")
+# data = pd.read_csv("stress.csv")
 
 print(data.shape)
 
@@ -15,23 +15,11 @@ print(data.shape)
 x = data.drop(columns=['sl'])  # Adjust 'target_column' to your target column name
 y = data['sl']
 
-print(x.shape, y.shape)
 
 data.rename(columns = {'rr':'respiration rate', 't':'body temperature',
                         'bo':'blood oxygen', 'sh':'sleeping hours'
                         ,'hr':'heart rate', 'sl':'stress level'}, inplace = True)
-print(data.head())
 
-print(data.describe().T)
-
-# checking if there is any null values
-print(data.isna().sum().sort_values())
-
-# printing the dataset columns
-print(data.columns)
-
-boxplot = data.boxplot(column=['respiration rate', 'body temperature', 'blood oxygen', 'sleeping hours', 'heart rate'],figsize=(15,5))
-print(boxplot)
 
 # scaling the features
 #importing library
@@ -40,7 +28,7 @@ from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 # transform data
 scaled = scaler.fit_transform(data[['respiration rate', 'body temperature', 'blood oxygen', 'sleeping hours','heart rate']])
-print(scaled)
+
 
 # chaniging the readings with the scaled features
 newdf = pd.DataFrame(scaled, columns =['respiration rate', 'body temperature', 'blood oxygen', 'sleeping hours', 'heart rate'])
@@ -50,14 +38,9 @@ newdf['stress level'] = data['stress level']
 newdf['stress level'].value_counts().sort_values()
 #The data is equally distributed among all the stress levels
 
-#plotting a pie chart to show the distribution of data
-label = ['low/normal' , 'medium low' , 'medium' ,'medium low','high']
-ex=[0.1,0,0,0,0.1]
-plt.pie(newdf['stress level'].value_counts(),labels=label,autopct='%.0f%%',explode=ex,shadow=True)
-plt.show()
 
 
-print(newdf.corrwith(newdf['stress level'], method = 'pearson'))
+newdf.corrwith(newdf['stress level'], method = 'pearson')
 
 #Importing the Models
 from sklearn.model_selection import train_test_split as tts
@@ -71,8 +54,6 @@ X = newdf[['respiration rate', 'body temperature', 'blood oxygen', 'sleeping hou
 y = newdf['stress level']
 #splitting among test and train dataset
 x_train, x_test, y_train, y_test= tts(X, y, test_size=0.4)
-print('Dimensions of train dataset:',x_train.shape)
-print('Dimensions of test dataset:',x_test.shape)
 
 #defining dictionaries for storing results of different models and comparing
 sc = {}
@@ -81,10 +62,10 @@ rn = {}
 #Logistic Regression
 lrr=LogisticRegression()
 lrr.fit(x_train,y_train)
-lrr_pred=lrr.predict(x_test)
-print('accuracy_score:',metrics.accuracy_score(y_test, lrr_pred))
 
+'''
 r=cvs(lrr, X, y, cv=10, scoring='accuracy').mean()
 sc['Logistic Regression']=r
 rn['Logistic Regression']=np.array(np.unique(lrr_pred, return_counts=True))
 print('cross val score:',r)
+'''
